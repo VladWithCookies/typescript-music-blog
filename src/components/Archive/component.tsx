e611fc7b7fadc04fa1fa5fa029bda9f435c7d36d
Vlad Viderko <summervibeswith9pm@gmail.com>
2019-11-28 12:45:45 +0200

    Add description

--- src/components/Archive/component.tsx
+++ src/components/Archive/component.tsx
@@ -5,10 +5,19 @@ import ArchiveItem from './ArchiveItem'
 import playlists from '../../data/playlists'
 
 const Archive = () => (
-  <Container className="archive">
+  <Container className="archive" textAlign="left">
     <h1 className="archive__header">
-      12 Tracks of Year
+      12 Tracks of the Year Project
     </h1>
+    <p>
+      The project was created at March, 2018. The main idea is to create playlists from 12 handpicked songs each year.
+      I pick one song each mounth starting from March and ending with February. Each song is my favorite songs of the mounth.
+      This song can be a fresh release of the mounth or hidden by decades treasure which was a music discovery of the mounth for me.
+      Mostly playlist contains songs in such genres as Downtempo, Hip-Hop, House but not only.
+    </p>
+    <h2 className="archive__header">
+      Archive
+    </h2>
     <Grid
       stackable
       columns={3}
